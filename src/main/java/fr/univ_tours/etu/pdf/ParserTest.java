527f8b50cbdd3cdffb775fda61dd343271f61d87
katsiaryna <katherine.krasnoschok@hotmail.com>
2016-03-15 02:26:35 +0100

    WordNet JWI testing

--- src/main/java/fr/univ_tours/etu/pdf/ParserTest.java
+++ src/main/java/fr/univ_tours/etu/pdf/ParserTest.java
@@ -1,5 +1,8 @@
 package fr.univ_tours.etu.pdf;
 
+import edu.mit.jwi.Dictionary;
+import edu.mit.jwi.IDictionary;
+import edu.mit.jwi.item.*;
 import org.apache.lucene.document.Document;
 import org.apache.pdfbox.cos.COSDocument;
 import org.apache.pdfbox.pdfparser.PDFParser;
@@ -7,8 +10,11 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.util.PDFTextStripper;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+
 
 /**
  * Created by Katherine on 10.03.2016.
@@ -40,14 +46,44 @@ public class ParserTest {
             pdfStripper.setStartPage(1);
             pdfStripper.setEndPage(10);
             Text = pdfStripper.getText(pdDoc);*/
+            testDictionary();
             Document luceneDocument = LucenePDFDocument.getDocument(file);
             System.out.println();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
     }
 
-}
+    public static void testDictionary() throws IOException{
+        String wnhome = System.getenv("WNHOME");
+        String path = wnhome + File.separator + "dict";
+        URL url = new URL("file", null, path);
+
+        IDictionary dictionary = new Dictionary(url);
+        dictionary.open();
+
+        IIndexWord indexWord = dictionary.getIndexWord("database", POS.NOUN);
+        IWordID wordID = indexWord.getWordIDs().get(0);
+        IWord word = dictionary.getWord(wordID);
+        System .out . println ("Id = " + wordID );
+        System .out . println (" Lemma = " + word . getLemma ());
+        System .out . println (" Gloss = " + word . getSynset (). getGloss ());
+
+        List<ISynsetID> hypernyms =
+            word.getSynset() . getRelatedSynsets ( Pointer . HYPERNYM );
+        // print out each h y p e r n y m s id and synonyms
+        List <IWord > words ;
+        for( ISynsetID sid : hypernyms ){
+            words = dictionary . getSynset (sid). getWords ();
+            System .out . print (sid + " {");
+            for(Iterator<IWord > i = words . iterator (); i. hasNext () ;){
+                System .out . print (i. next (). getLemma ());
+                if(i. hasNext ())
+                    System .out . print (", ");
+                }
+            System .out . println ("}");
+            }
+        }
+    }
+
+
